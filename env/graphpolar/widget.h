e2b7c287d2e63d245adba938e0768c164841526d
sven4500 <sven4500@mail.ru>
2021-05-18 00:57:33 +0300

    Add polar graph demo.

--- env/graphpolar/widget.h
+++ env/graphpolar/widget.h
@@ -16,6 +16,19 @@ public:
     {
         _graph = new GraphPolar<float>(this);
 
+        QPalette pal = _graph->palette();
+        pal.setColor(QPalette::Window, Qt::lightGray);
+
+        // You can make a darker background to get more contrast.
+        //_graph->setPalette(pal);
+
+        // Let us see the difference between curves with different
+        // discretization levels.
+        addCurve(_graph, 10, Qt::darkMagenta);
+        addCurve(_graph, 20, Qt::darkGreen);
+        addCurve(_graph, 100, Qt::blue);
+        addCurve(_graph, 2000, Qt::red);
+
         QGridLayout* const layout = new QGridLayout(this);
         //layout->setMargin(0);
         layout->setSpacing(0);
@@ -32,6 +45,24 @@ public:
     }
 
 private:
+    static void addCurve(GraphPolar<float>* graph, int size, QColor const& color)
+    {
+        QVector<float> rho(size);
+        QVector<float> phi(size);
+
+        float const stepRho = 1.0 / size;
+        float const stepPhi = (4.0 * M_PI) / size;
+
+        for(auto i = 0; i < size; ++i)
+        {
+            rho[i] = stepRho * i;
+            phi[i] = stepPhi * i;
+        }
+
+        auto const id = graph->addCurve(rho, phi);
+        graph->setColor(id, color);
+    }
+
     GraphPolar<float>* _graph;
 
 };
